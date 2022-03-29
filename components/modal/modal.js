9416b2bbef573e78212428ccfa54f63eafb520db
Shubhamkr2610 <shubham1bcabnc18@gmail.com>
2022-03-29 22:13:36 +0530

    created: modal component

---
+++ components/modal/modal.js
@@ -0,0 +1,21 @@
+const openModal = document.querySelector('#open-modal');
+const modal = document.querySelector('#modal');
+closeBtnTop = document.querySelector('#close');
+closeBtnBottom = document.querySelector('#close-button')
+
+openModal.addEventListener('click' , () => {
+
+  modal.style.display="flex"
+})
+
+closeBtnTop.addEventListener('click', () => {
+    modal.style.display="none"
+})
+closeBtnBottom.addEventListener('click', () => {
+    modal.style.display="none"
+})
+window.onclick = (event) => {
+    if(event.target == modal) {
+        modal.style.display="none"
+    }
+}
